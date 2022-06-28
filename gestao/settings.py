82e293018430fb642d669700fb0d6c70ca11e7cd
SKTNightmareT1 <nightmaregamer11@outlook.com>
2022-06-28 13:07:53 -0300

    'ajuste'

--- gestao/settings.py
+++ gestao/settings.py
@@ -1,3 +1,5 @@
+from email.mime import base
+import os
 """
 Django settings for gestao project.
 
@@ -117,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
